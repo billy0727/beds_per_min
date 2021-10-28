3c1571f471e2a3ae460e6cceba703258b55adf7e
billy0727 <billy0727@gmail.com>
2021-10-28 10:16:54 +0800

    Update beds.sh
    add crontab command in beds.sh

--- beds.sh
+++ beds.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo sh -c "echo 'select id, alert_ts, resident_id, pad_mac, alert_type_id from beds' | mysql -u root -pbds316 -A gv > /home/mwg/mwg_jubo/web_backend/static/beds.txt"
+
+
+# add crontab 
+# */1 * * * * root /bin/bash /home/mwg/beds_per_min/beds.sh
